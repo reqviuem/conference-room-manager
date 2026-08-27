695dc12b317770276e2842eff87a9e2435bc169f
reqvi <slavil.larin@gmail.com>
2026-08-27 19:10:45 +0200

    Add AvailableServices model with predefined service list

---
+++ RoomManager/Models/AvailableServices.cs
@@ -0,0 +1,11 @@
+﻿namespace RoomManager.Models;
+
+public static class AvailableServices
+{
+    public static readonly Dictionary<string, int> Services = new Dictionary<string, int>
+    {
+        { "Проєктор", 500 },
+        { "Wi-Fi", 300 },
+        { "Звук", 700 }
+    };
+}
