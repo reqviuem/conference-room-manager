9d81c702e1c3d8bebcfb902e42241c4d4e5302fa
reqvi <slavil.larin@gmail.com>
2026-08-27 17:33:18 +0200

    Add EF Core DbContext and Room model

---
+++ RoomManager/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RoomManager.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<DbContext> options) : base(options)
+    {
+        
+    }
+}
---
+++ RoomManager/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomManager.Models;
+
+public class Room
+{
+    public Guid Id { get; set; }
+
+    [Required] public string Name { get; set; } = null!;
+    
+    [Required] public int Capacity { get; set; }
+
+    [Required] public List<string> AvailableServices { get; set; } = null!;
+    
+    [Required] public int PricePerHour { get; set; }
+    
+    
+}
