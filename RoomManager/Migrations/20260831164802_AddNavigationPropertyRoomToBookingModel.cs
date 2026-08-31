63ecca3e2ce9f84f21388b796fa04185eb6bc6a3
reqvi <slavil.larin@gmail.com>
2026-08-31 20:29:47 +0200

    Add navigation property `Room` to `Booking` model, enhance database schema with foreign key constraint, and adjust `Room` seed data with updated GUIDs

---
+++ RoomManager/Migrations/20260831164802_AddNavigationPropertyRoomToBookingModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RoomManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNavigationPropertyRoomToBookingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "Capacity", "Name", "PricePerHour" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), 50, "Зал А", 2000m },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), 100, "Зал B", 3500m },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), 30, "Зал C", 1500m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_RoomId",
+                table: "Bookings",
+                column: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Rooms_RoomId",
+                table: "Bookings",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Rooms_RoomId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_RoomId",
+                table: "Bookings");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
+        }
+    }
+}
---
+++ RoomManager/Migrations/20260831165105_AdjustSeedIdOfRooms.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RoomManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustSeedIdOfRooms : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "Capacity", "Name", "PricePerHour" },
+                values: new object[,]
+                {
+                    { new Guid("a2222222-2222-2222-2222-222222222222"), 100, "Зал B", 3500m },
+                    { new Guid("b1111111-1111-1111-1111-111111111111"), 50, "Зал А", 2000m },
+                    { new Guid("c3333333-3333-3333-3333-333333333333"), 30, "Зал C", 1500m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("a2222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("b1111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "Id",
+                keyValue: new Guid("c3333333-3333-3333-3333-333333333333"));
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "Id", "Capacity", "Name", "PricePerHour" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), 50, "Зал А", 2000m },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), 100, "Зал B", 3500m },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), 30, "Зал C", 1500m }
+                });
+        }
+    }
+}
